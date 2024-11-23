744b562eed6faa94a3213c70cbe3274688c97da9
George-Markas <georgemarkanton@proton.me>
2024-11-23 05:40:33 +0200

    More fixes

--- src/main.c
+++ src/main.c
@@ -37,22 +37,40 @@ int main(int argc, char* argv[]) {
             MPI_Send(&sequence_length, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
 
-        int index;
-        int order_breaks_after = -1;
+        int report;
+        int index = 0;
+        int* fail_points = calloc(process_count, sizeof(int));
+
+        // Receive "points" (sequence indexes) from processes.
         for(int i = 1; i < process_count; i++) {
-            MPI_Recv(&index, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            if(index >= 0)
-                order_breaks_after = index;
+            MPI_Recv(&report, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if(report >= 0) {
+                fail_points[index] = report;
+                index++;
+            }
         }
 
-        if(order_breaks_after == -1) {
+        if(!index) {
             puts("Array is in ascending order\n");
         }
-        else
-            printf("\nSequence is not in ascending order, breakpoint at: %d\n", order_breaks_after);
+        else {
+            int first_to_fail = fail_points[0];
+
+            for(int i = 1; i < index; i++) {
+                if(fail_points[i] < first_to_fail) {
+                    first_to_fail = fail_points[i];
+                }
+            }
+
+            printf("\nSequence is not in ascending order, breakpoint at: %d\n", first_to_fail);
+
+        }
 
         free(sequence);
         sequence = NULL;
+
+        free(fail_points);
+        fail_points = NULL;
     }
     else {
         int order_breaks_after = -1;
@@ -70,13 +88,15 @@ int main(int argc, char* argv[]) {
         for(int i = process_rank - 1; i < sequence_length - 1; i += (process_count - 1)) {
             // if nth element is less than n+1th element (aka not in ascending order).
             if(sequence[i] > sequence[i + 1]) {
-                printf("Rank: %d compares: %d - %d\n", process_rank, sequence[i], sequence[i + 1]);
+                printf("\nRank %d compares: %d - %d\n", process_rank, sequence[i], sequence[i + 1]);
 				order_breaks_after = i;
+                break;
             }
         }
 
         // Send back (to process 0) the index of the sequence at which the ascending order is no longer adhered to.
         MPI_Send(&order_breaks_after, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
         free(sequence);
         sequence = NULL;
     }
