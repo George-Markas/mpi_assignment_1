8fd978993cce0d9538e046f19edccd5dfde0477a
George-Markas <georgemarkanton@proton.me>
2024-11-28 01:01:19 +0200

    Added source file

---
+++ src/input_sanitizer.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <ctype.h>
+
+/* Provides validity checking for read_int() */
+int input_sanitizer(const char *buffer, int allow_negative) {
+
+    // In case of empty input, even if NULL macro is not defined
+    if(buffer == NULL || *buffer == '\0')
+        return 0;
+
+    // Handle sign
+    int i = 0;
+    if(buffer[0] == '-') {
+        if(allow_negative) {
+            i++;
+        } else {
+            return 0;
+        }
+    }
+
+    for(; buffer[i] != '\n'; i++) {
+        if(!isdigit(buffer[i]))
+            return 0;
+    }
+
+    return 1;
+}
